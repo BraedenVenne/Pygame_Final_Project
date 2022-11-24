a6b4b1c1544555c364ad15aeda2824299ef437f3
Braeden <bvenne@my.bcit.ca>
2022-11-24 11:04:04 -0800

    updated game.py

--- screens/game.py
+++ screens/game.py
@@ -1,20 +1,20 @@
 import pygame
 from screens.base_screen import BaseScreen
-from components.player import Player
-from components.globals import SCREEN_WIDTH, SCREEN_HEIGHT
+from components import Player, SCREEN_WIDTH, SCREEN_HEIGHT
 
 class GameScreen(BaseScreen):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.sky_surf = pygame.image.load('images/sky.png')
-        self.player = Player(SCREEN_WIDTH // 2, SCREEN_HEIGHT - 150)
+        self.player = Player(SCREEN_WIDTH // 2, SCREEN_HEIGHT - 100)
 
     def update(self):
         self.player.movement()
         
-
     def draw(self):
         self.window.blit(self.sky_surf, (0, 0))
         self.player.update(self.window)
+
+
     
         
