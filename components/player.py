94b183d0f37c0939adad03da978b36b6de4cdfe6
Braeden <bvenne@my.bcit.ca>
2022-11-23 19:06:05 -0800

    Added screen boundaries for the character

--- components/player.py
+++ components/player.py
@@ -1,19 +1,41 @@
 import pygame
+from components.globals import SCREEN_WIDTH, SCREEN_HEIGHT
 
 class Player(pygame.sprite.Sprite):
     def __init__(self, x, y):
         super().__init__()
-        homer_surf = pygame.image.load('images/jump.png')
+        homer_surf = pygame.image.load('images/jump.png').convert_alpha()
         self.homer = pygame.transform.rotozoom(homer_surf, 0, 0.75)
         self.rect = self.homer.get_rect(center=(x, y))
+        self.flip = False
         
-    def key_input(self):
+    def movement(self):
+        """
+        Handles the player's movement
+        """
+        # distance variables
+        x = 0
+        y = 0
+
         keys = pygame.key.get_pressed()
         if keys[pygame.K_a]:
-            self.rect.x -= 10
+            x = -10
+            self.flip = True
         if keys[pygame.K_d]:
-            self.rect.x += 10
-    
+            x = 10
+            self.flip = False
+
+        # Screen boundaries
+        if self.rect.x > SCREEN_WIDTH:
+            self.rect.x = 0
+        elif self.rect.x < 0:
+            self.rect.x = SCREEN_WIDTH
+        
+        # screen position
+        self.rect.x += x
+        self.rect.y += y
+
+
     def update(self, window):
-        window.blit(self.homer, self.rect)
+        window.blit(pygame.transform.flip(self.homer, self.flip, False), self.rect)
 
