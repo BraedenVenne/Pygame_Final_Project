8ec5e6a229e8605e0dc9548aa5934d2c19d621f7
Braeden <bvenne@my.bcit.ca>
2022-11-25 14:03:22 -0800

    added a shoot function that creates instances of projectiles, created a remove function to remove fireballs

--- screens/game.py
+++ screens/game.py
@@ -1,8 +1,7 @@
 import pygame
 import random
 from screens import BaseScreen
-from components import Player, MAX_WIDTH, MAX_HEIGHT, Balloon
-
+from components import Player, MAX_WIDTH, MAX_HEIGHT, Balloon, Projectile
 
 class GameScreen(BaseScreen):
     def __init__(self, *args, **kwargs):
@@ -10,15 +9,38 @@ class GameScreen(BaseScreen):
         self.sky_surf = pygame.image.load('images/sky.png')
         self.player = Player(MAX_WIDTH / 2, MAX_HEIGHT - 50)
         self.balloon = Balloon(MAX_WIDTH / 2, 130)
+        self.projectiles = []
+        self.player_health = 20
+
+    def shoot(self):
+        x_pos = self.balloon.rect.center[0]
+        chance = random.randint(1, 50)
+        if chance == 1:
+            projectile = Projectile(x_pos, 200)
+            projectile.update(self.window)
+            self.projectiles.append(projectile)
 
     def update(self):
         self.player.movement()
         self.balloon.movement()
-        
+        self.shoot()
+        self.remove()
+
     def draw(self):
         self.window.blit(self.sky_surf, (0, 0))
         self.player.update(self.window)
         self.balloon.update(self.window)
+        for p in self.projectiles:
+            p.update(self.window)
+
+    def remove(self):
+        for p in self.projectiles:
+            if p.rect.y > 600:
+                self.projectiles.remove(p)
+            if p.rect.colliderect(self.player.rect):
+                self.projectiles.remove(p)
+                print('hit')
+                self.player_health -= 1
     
     def manage_event(self, event):
         if event.type == pygame.QUIT:
@@ -27,6 +49,3 @@ class GameScreen(BaseScreen):
         elif event.type == pygame.KEYDOWN and event.key == pygame.K_ESCAPE:
             self.next_screen = False
             self.running = False
-
-
-        
