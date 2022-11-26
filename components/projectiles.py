cebc3bc3e444d6343ca28dfbd40e76332514840c
Braeden <bvenne@my.bcit.ca>
2022-11-25 17:20:13 -0800

    added a donut projectile

--- components/projectiles.py
+++ components/projectiles.py
@@ -3,25 +3,20 @@ import pygame
 from .globals import MAX_HEIGHT
 
 class Projectile(pygame.sprite.Sprite):
-    def __init__(self, x, y):
+    def __init__(self, x: int, y: int, projectile_type: str):
         super().__init__()
-        self.image = pygame.image.load('images/fireball.png')
-        self.image = pygame.transform.rotozoom(self.image,0,0.5)
+        self.projectile_type = projectile_type
+        if self.projectile_type == 'fireball':
+            self.image = pygame.image.load('images/fireball.png')
+            self.image = pygame.transform.rotozoom(self.image,0,0.5)
+        else:
+            self.image = pygame.image.load('images/donut.png')
+            self.image = pygame.transform.rotozoom(self.image,0,0.1)
         self.rect = self.image.get_rect()
         self.rect.x = x
         self.rect.y = y
-    
-    # def shoot_projectile(self):
-    #     y = 5
-    #     self.rect.y += y
-
-    def remove(self):
-        if self.rect.y >= 100:
-            self.kill()
-
-
-
+ 
     def update(self, window):
         self.rect.y += 5
         window.blit(self.image, self.rect)
-        self.remove()
+        
