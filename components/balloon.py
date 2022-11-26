2d6d84bb25fe5d81a4effa0531a6ff676b9930a2
Braeden <bvenne@my.bcit.ca>
2022-11-25 16:37:02 -0800

    added a speed variable

--- components/balloon.py
+++ components/balloon.py
@@ -1,6 +1,4 @@
 import pygame
-from components import MAX_WIDTH
-from .projectiles import Projectile
 
 class Balloon(pygame.sprite.Sprite):
     def __init__(self, x, y):
@@ -10,25 +8,21 @@ class Balloon(pygame.sprite.Sprite):
         self.image = pygame.transform.flip(self.image, True, False)
         # create a rectangle and set the position to the top, center of screen
         self.rect = self.image.get_rect(center=(x,y))
-        self.x = 1
+        self.speed = 1
         self.flip = False
-
-
-
-    # def shoot_projectile(self):
-    #     self.projectile.rect.center = self.rect.center
-
 
     def movement(self):
         # make the balloon automatically move to the right or left
-        self.rect.x += self.x
+        self.rect.x += self.speed
 
         # if the balloon reaches the edge of the screen, change direction and flip the image
-        if self.rect.left >= 300 or self.rect.right <= 100:
-            self.x *= -1
+        if self.rect.left + self.speed >= 300 or self.rect.right +self.speed <= 100:
+            self.speed *= -1
             self.flip = not self.flip
         
-    def update(self, window):
+    def update(self):      
+        self.movement()
+    
+    def draw(self, window):
         window.blit(pygame.transform.flip(self.image, self.flip, False), self.rect)
-        # self.projectiles.update(window)
         
