437e38e1e0de06446d840e3990ed5b2bf4a05d3c
Braeden <bvenne@my.bcit.ca>
2022-12-04 04:21:07 -0800

    cleaned up code, changed how score is calculated

--- screens/game.py
+++ screens/game.py
@@ -4,31 +4,41 @@ from screens import BaseScreen
 from components import Player, MAX_WIDTH, MAX_HEIGHT, Balloon, Projectile
 
 class GameScreen(BaseScreen):
+    """
+    This is the main game loop. It will run until the user dies or quits the game.
+    """
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.sky_surf = pygame.image.load('images/background.png')
         self.player = Player(MAX_WIDTH / 2, MAX_HEIGHT - 50)
         self.balloon = Balloon(MAX_WIDTH / 2, 130)
         self.projectiles = []
-        self.player_health = 10
-        self.test_font = pygame.font.Font('font/Pixeltype.ttf', 50)
-        self.start_time = 0
+        self.player_health = 5
         self.health_text = self.test_font.render(f'Health: {self.player_health}', True, (0, 0, 0))
-        self.score = 0
-        self.start_time = pygame.time.get_ticks() // 1000
         self.speed_increase = 10
         self.balloon_speed = 1
+        self.donut_count = 0
+        self.final_score = 0
+
 
     def get_score(self):
+        """
+        This method will get the score of the player and increase the speed of the balloon based on the score.
+        """
         self.score = (pygame.time.get_ticks() // 1000) - self.start_time
-        # if score is greater than speed increase and balloon speed is >
+
+    def balloon_speed_increase(self):
         if self.score > self.speed_increase and self.score > 0 and self.balloon.speed > 0:
             self.balloon.speed -= 1
             self.balloon_speed += 1
             self.balloon.speed = self.balloon_speed
-            self.speed_increase *= 2
+            self.speed_increase += 20
 
-    def shoot(self):
+    def shoot_chance(self):
+        """
+        This method will randomly generate a number between 1 and 100. 
+        If the number is 1, then the balloon will shoot a projectile.
+        """
         x_pos = self.balloon.rect.center[0]
         chance = random.randint(1, 100)
         if chance % 15 == 0:
@@ -41,6 +51,9 @@ class GameScreen(BaseScreen):
             self.projectiles.append(projectile)
 
     def draw(self):
+        """
+        This method will draw the background, player, balloon, and projectiles to the screen.
+        """
         self.window.blit(self.sky_surf, (0, 0))
         self.player.update(self.window)
         self.balloon.draw(self.window)
@@ -51,7 +64,10 @@ class GameScreen(BaseScreen):
         # draw the score to the top left of the screen
         self.window.blit(self.test_font.render(f'Score: {self.score}', True, (0, 0, 0)), (10, 10))
 
-    def remove_projectile(self):
+    def shoot(self):
+        """
+        This method handles the projectile collisions. 
+        """
         for p in self.projectiles:
             if p.rect.y > 600:
                 self.projectiles.remove(p)
@@ -63,28 +79,35 @@ class GameScreen(BaseScreen):
                 self.player_health -= 1
                 self.health_text = self.test_font.render(f'Health: {self.player_health}', True, (0, 0, 0))
                 if self.player_health == 0:                 
-                    self.start_time += pygame.time.get_ticks() // 1000
-                    self.score = 0
                     # change the screen to the game over screen
+                    self.final_score = self.score
                     self.next_screen = 'game_over'
                     self.running = False
                     
             elif p.rect.colliderect(self.player.rect) and p.projectile_type == 'donut':
-                chance = random.randint(1, 2)
-                if chance == 1:
-                    pygame.mixer.Sound('audio/mmmdonuts.wav').play()
                 self.projectiles.remove(p)
-                self.player_health += 1
-                self.health_text = self.test_font.render(f'Health: {self.player_health}', True, (0, 0, 0))
+                self.donut_count += 1
+                if self.donut_count == 2:
+                    pygame.mixer.Sound('audio/mmmdonuts.wav').play()
+                    self.player_health += 1
+                    self.health_text = self.test_font.render(f'Health: {self.player_health}', True, (0, 0, 0))
+                    self.donut_count = 0
 
     def update(self):
+        """
+        This function will update the game screen.
+        """
+        self.shoot_chance()
         self.shoot()
-        self.remove_projectile()
         self.get_score()
+        self.balloon_speed_increase()
         self.player.movement()
         self.balloon.update()
         
     def manage_event(self, event):
+        """
+        This function will handle the events for the game screen.
+        """
         if event.type == pygame.QUIT:
             self.next_screen = False
             self.running = False
