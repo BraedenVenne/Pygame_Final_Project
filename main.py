0305b72384d88cf5319db5b105facdaa8b99c756
Braeden <bvenne@my.bcit.ca>
2022-12-04 04:22:25 -0800

    can now pass a final score to game over screen

--- main.py
+++ main.py
@@ -1,19 +1,19 @@
 import pygame
-from  screens import StartScreen, GameScreen, GameOver
+from  screens import StartScreen, GameScreen, GameOver, LoginScreen
 from components import MAX_WIDTH, MAX_HEIGHT
 
 class Game:
     """Main class for the application"""
-
     def __init__(self):
         # Creates the window
         self.window = pygame.display.set_mode((MAX_WIDTH, MAX_HEIGHT))
     def run(self):
-        pygame.mixer.init()
+        pygame.init()
         """Main method, manages interaction between screens"""
 
         # These are the available screens
         screens = {
+            "login": LoginScreen,
             "start": StartScreen,
             "game": GameScreen,
             "game_over": GameOver,
@@ -21,6 +21,7 @@ class Game:
 
         # Start the loop
         running = True
+        final_score = 0
         current_screen = "start"
         while running:
             # Obtain the screen class
@@ -29,8 +30,12 @@ class Game:
                 raise RuntimeError(f"Screen {current_screen} not found!")
 
             # Create a new screen object, "connected" to the window
-            screen = screen_class(self.window)
+            if current_screen == "game_over":
+                screen = screen_class(self.window, final_score)
+            else:
+                screen = screen_class(self.window)
 
+                
             # Run the screen
             screen.run()
             # When the `run` method stops, we should have a `next_screen` setup
@@ -38,6 +43,9 @@ class Game:
                 running = False
             # Switch to the next screen
             current_screen = screen.next_screen
+
+            if screen.final_score != 0:
+                final_score = screen.final_score
 
 if __name__ == "__main__":
     donut_jump = Game()
