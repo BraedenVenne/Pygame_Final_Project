8c167384507530298106afa2b95a203d17a4c0db
Braeden <bvenne@my.bcit.ca>
2022-12-04 04:24:15 -0800

    added some docstrings

--- components/balloon.py
+++ components/balloon.py
@@ -1,6 +1,10 @@
 import pygame
 
 class Balloon(pygame.sprite.Sprite):
+    """
+    This is the Balloon class. It handles
+    the balloon's movement and animation.
+    """
     def __init__(self, x, y):
         super().__init__()
         self.image = pygame.image.load('images/balloon.png')
@@ -12,6 +16,9 @@ class Balloon(pygame.sprite.Sprite):
         self.flip = False
 
     def movement(self):
+        """
+        This method handles the balloon's movement.
+        """
         # make the balloon automatically move to the right or left
         self.rect.x += self.speed
 
@@ -20,9 +27,15 @@ class Balloon(pygame.sprite.Sprite):
             self.speed *= -1
             self.flip = not self.flip
         
-    def update(self):      
+    def update(self):  
+        """
+        This method updates the balloon's movement and animation.
+        """    
         self.movement()
     
     def draw(self, window):
+        """
+        This method draws the balloon to the screen.
+        """
         window.blit(pygame.transform.flip(self.image, self.flip, False), self.rect)
         
