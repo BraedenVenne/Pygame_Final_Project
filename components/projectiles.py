2211a2dd7481ba1bb09ca14e59cbb89685ea0a0c
Braeden <bvenne@my.bcit.ca>
2022-12-04 21:53:21 -0800

    cleaned up code, added docstrings

--- components/projectiles.py
+++ components/projectiles.py
@@ -1,8 +1,15 @@
 import pygame
 
 class Projectile(pygame.sprite.Sprite):
+    """
+    This class represents the projectiles.
+
+    Args:
+        pygame (_type_): This is the pygame module.
+    """
     def __init__(self, x: int, y: int, projectile_type: str):
         super().__init__()
+        # projectile type
         self.projectile_type = projectile_type
         if self.projectile_type == 'fireball':
             self.image = pygame.image.load('images/fireball.png')
@@ -13,6 +20,13 @@ class Projectile(pygame.sprite.Sprite):
         self.rect = self.image.get_rect(center=(x,y))
  
     def update(self, window):
+        """
+        This method updates the projectile's position.
+
+        Args:
+            window (_type_): This is the window object.
+        """
+        # move the projectile
         self.rect.y += 5
         window.blit(self.image, self.rect)
         
