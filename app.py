222de619bb81980ccbcc9e4782a20a6960cb0f60
BraedenVenne <100611781+BraedenVenne@users.noreply.github.com>
2022-12-04 23:34:11 -0800

    removed line old line of code from the file

--- app.py
+++ app.py
@@ -2,14 +2,12 @@ from flask import Flask, render_template
 from models.scores import Score
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = "asdgjlasdhajskhiewofhdskjbnczd"
 
 # route for the home page
 @app.route('/')
 def scoreboard():
     score = Score('scores.json')
     return render_template('scoreboard.html', score=score)
-
 
 if __name__ == '__main__':
     app.run(debug=True)
