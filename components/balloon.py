f003ffd04668339d2888dc99b64ea76bb0e2263a
Braeden <bvenne@my.bcit.ca>
2022-12-04 21:52:49 -0800

    cleaned up code, added docstrings

--- components/balloon.py
+++ components/balloon.py
@@ -2,15 +2,17 @@ import pygame
 
 class Balloon(pygame.sprite.Sprite):
     """
-    This is the Balloon class. It handles
-    the balloon's movement and animation.
+    This class represents the balloon enemy.
+
+    Args:
+        pygame (_type_): This is the pygame module.
     """
     def __init__(self, x, y):
         super().__init__()
+        # balloon image
         self.image = pygame.image.load('images/balloon.png')
         self.image = pygame.transform.rotozoom(self.image,0,0.45)
         self.image = pygame.transform.flip(self.image, True, False)
-        # create a rectangle and set the position to the top, center of screen
         self.rect = self.image.get_rect(center=(x,y))
         self.speed = 1
         self.flip = False
@@ -19,10 +21,10 @@ class Balloon(pygame.sprite.Sprite):
         """
         This method handles the balloon's movement.
         """
-        # make the balloon automatically move to the right or left
+        # automatically move the balloon to the right or left
         self.rect.x += self.speed
 
-        # if the balloon reaches the edge of the screen, change direction and flip the image
+        # cahnge the direction of the balloon when it hits the edge of the screen
         if self.rect.left + self.speed >= 300 or self.rect.right +self.speed <= 100:
             self.speed *= -1
             self.flip = not self.flip
